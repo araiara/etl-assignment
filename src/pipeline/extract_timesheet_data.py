241bb08b17311f11466a337a35009bdb7a394bb0
Arsana Rai <arsanaraio.o@gmail.com>
2021-09-15 08:05:03 +0545

    update extract from csv

--- src/pipeline/extract_timesheet_data.py
+++ src/pipeline/extract_timesheet_data.py
@@ -9,51 +9,73 @@ def connect():
                 database = "employee_timesheet_db"
             )
 
-def extract_timesheet_data(file_path):
+# from src.utils import connect
+
+def create_raw_timesheet_data_schema():
+    conn = connect()
+    cursor = conn.cursor()
+
+    with open('../../schema/create_raw_timesheet_data.sql') as create_file:
+        create_query = "".join(create_file.readlines())
+        cursor.execute(create_query)
+        conn.commit()
+    cursor.close()
+    conn.close()
+
+
+def extract_timesheet_data(file_path, flag):
     conn = connect()
     cursor = conn.cursor()
 
     with open(file_path, 'r') as file:
-
-        delete_query = "DELETE FROM raw_timesheet_data;"
-        cursor.execute(delete_query)
-        conn.commit()
+        if flag:
+            with open('../sql/procedure/delete_raw_timesheet_data.sql') as delete_file:
+                delete_query = "".join(delete_file.readlines())
+                cursor.execute(delete_query)
+                conn.commit()
 
         next(file)
         for line in file:
             row = line.strip().split(',')
 
-            with open('../sql/procedure/insert_timesheet_data.sql') as insert_file:
+            with open('../sql/procedure/insert_raw_timesheet_data.sql') as insert_file:
                 insert_query = "".join(insert_file.readlines())
                 cursor.execute(insert_query, row)
                 conn.commit()
     cursor.close()
     conn.close()
 
-# def extract_timesheet_data_copy(file_path):
-#     conn = connect()
-#     cursor = conn.cursor()
+def extract_timesheet_data_copy(file_path, flag):
+    conn = connect()
+    cursor = conn.cursor()
 
-#     delete_query = "DELETE FROM raw_timesheet_data;"
-#     cursor.execute(delete_query)
-#     conn.commit()
+    if flag:
+        with open('../sql/procedure/delete_raw_timesheet_data.sql') as delete_file:
+            delete_query = "".join(delete_file.readlines())
+            cursor.execute(delete_query)
+            conn.commit()
 
-#     copy_query = '''
-#         copy raw_timesheet_data
-#         FROM %s
-#         WITH (
-#             FORMAT TEXT,
-#             DELIMITER ','
-#         );
-#     '''
+    copy_query = '''
+        COPY raw_timesheet_data
+        FROM %s
+        WITH (
+            FORMAT TEXT,
+            DELIMITER ','
+        );
+    '''
 
-#     cursor.execute(copy_query, (file_path,))
-#     conn.commit()
-#     cursor.close()
-#     conn.close()
-    
+    cursor.execute(copy_query, (file_path,))
+    conn.commit()
+    cursor.close()
+    conn.close()
+ 
 if __name__ == "__main__":
-    timesheet_data_path = ['../../data/timesheet_2021_05_23.csv']
+    create_raw_timesheet_data_schema()
+
+    timesheet_data_path = ['../../data/timesheet_2021_05_23.csv', '../../data/timesheet_2021_06_23.csv', '../../data/timesheet_2021_07_24.csv']
     
     for path in timesheet_data_path:
-        extract_timesheet_data(path)
+        if path == timesheet_data_path[0]:
+            extract_timesheet_data(path, True)
+        else:
+            extract_timesheet_data(path, False)
