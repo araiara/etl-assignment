1f9b176e047c4b469b2f4611e3685fcf40d78faa
Arsana Rai <arsanaraio.o@gmail.com>
2021-09-15 08:06:14 +0545

    add schemas

---
+++ schema/create_raw_employee_data.sql
@@ -0,0 +1,16 @@
+CREATE table raw_employee (
+    employee_id TEXT,
+    first_name TEXT,
+    last_name TEXT,
+    department_id TEXT,
+    department_name TEXT,
+    manager_employee_id TEXT,
+    employee_role TEXT,
+    salary TEXT,
+    hire_date TEXT,
+    terminated_date TEXT,
+    terminated_reason TEXT,
+    dob TEXT, 
+    fte TEXT,
+    location TEXT
+);
---
+++ schema/create_raw_timesheet_data.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS raw_timesheet_data (
+	employee_id	TEXT,
+	cost_center	TEXT,
+	punch_in_time TEXT,	
+	punch_out_time TEXT,
+	punch_apply_date TEXT,
+	hours_worked TEXT,
+	paycode TEXT
+);
