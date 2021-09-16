838cebecf6b94328ce70b0767f96b9f9e7301707
Arsana Rai <arsanaraio.o@gmail.com>
2021-09-16 12:19:04 +0545

    update indentations

--- schema/create_raw_timesheet_data_archive.sql
+++ schema/create_raw_timesheet_data_archive.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS raw_timesheet_data_archive (
-	employee_id	VARCHAR(500),
-	cost_center	VARCHAR(500),
-	punch_in_time VARCHAR(500),	
-	punch_out_time VARCHAR(500),
-	punch_apply_date VARCHAR(500),
-	hours_worked VARCHAR(500),
-	paycode VARCHAR(500),
-    file_name VARCHAR(500)
+  employee_id	VARCHAR(500),
+  cost_center	VARCHAR(500),
+  punch_in_time VARCHAR(500),	
+  punch_out_time VARCHAR(500),
+  punch_apply_date VARCHAR(500),
+  hours_worked VARCHAR(500),
+  paycode VARCHAR(500),
+  file_name VARCHAR(500)
 );
